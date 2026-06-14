e1612d574857fdf1442464622923f1a4dead15d2
saulfabre <saulfabre2@gmail.com>
2026-06-14 13:23:11 -0400

    Implementado clase Main

--- Ejercicio2/src/Main.java
+++ Ejercicio2/src/Main.java
@@ -1,7 +1,33 @@
 public class Main {
 
     public static void main(String[] args) {
-        
-    }
-    
+
+        try {
+
+            CorredorHilo corredor1 = new CorredorHilo("Maria");
+            CorredorHilo corredor2 = new CorredorHilo("Pedro");
+            CorredorHilo corredor3 = new CorredorHilo("Juan");
+
+            System.out.println("Estado inicial: ");
+            System.out.println(corredor1.getState());
+            System.out.println(corredor2.getState());
+            System.out.println(corredor3.getState() + "\n");
+
+            corredor1.start();
+            corredor2.start();
+            corredor3.start();
+
+            Thread.sleep(40000);
+
+            System.out.println("Estado final: ");
+            System.out.println(corredor1.getState());
+            System.out.println(corredor2.getState());
+            System.out.println(corredor3.getState());
+        }
+
+        catch (Exception e) {
+
+            e.printStackTrace();
+        }   
+    } 
 }
