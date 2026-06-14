e6715aec4c11003c82b6c0b29ae57c1e6de2ba04
saulfabre <saulfabre2@gmail.com>
2026-06-14 12:08:55 -0400

    Implementado clase Main

--- Ejercicio1/src/Main.java
+++ Ejercicio1/src/Main.java
@@ -2,5 +2,20 @@ public class Main {
     
     public static void main(String[] args) {
         
+        try {
+
+            DescargaHilo archivo1 = new DescargaHilo("Archivo1");
+            DescargaHilo archivo2 = new DescargaHilo("Archivo2");
+            DescargaHilo archivo3 = new DescargaHilo("Archivo3");
+
+            archivo1.start();
+            archivo2.start();
+            archivo3.start();
+        }
+
+        catch (Exception e) {
+
+            e.printStackTrace();
+        }
     }
 }
