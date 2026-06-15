dde44cc174d6f5551a60c98ade7a86873ec9a314
saulfabre <saulfabre2@gmail.com>
2026-06-15 00:05:04 -0400

    Implementado clase Main

--- Ejercicio3/src/Main.java
+++ Ejercicio3/src/Main.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
 
+    public static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        
+
+        try {
+
+            System.out.print("Ingrese su edad: ");
+            int edad = scanner.nextInt();
+
+            UsuarioService.validarEdad(edad);
+        }
+
+        catch (Exception e) {
+
+            System.out.println(e.getMessage());
+        }  
     }  
 }
